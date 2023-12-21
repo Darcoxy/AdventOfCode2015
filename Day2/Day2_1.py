be600aea799e0d537f31c3858bbc6e8e0f43559e
JJ Lewandowski <post.lewandowski@gmail.com>
2023-12-21 15:40:49 +0000

    attempt 2

--- Day2/Day2_1.py
+++ Day2/Day2_1.py
@@ -6,22 +6,23 @@ with open(r"C:\Users\postl\Desktop\Personal\Projects\AdventOfCode2015\Day2\Day2_
         dimensions.append(line)
 
 for element in dimensions:
-    #dimenions = element.split('x')
     converted = list(map(int, element.split('x')))
     length = converted[0]
     width = converted[1]
     height = converted[2]
+
+    sides = [length * width, width * height, height * length]
+    smallestSide = min(sides)
 
     calculations = ['', '', '']
     calculations[0] = (2 * length * width)
     calculations[1] = (2 * width * height)
     calculations[2] = (2 * height * length)
 
-    slack = min(calculations)
+    slack = smallestSide
 
     sum = calculations[0] + calculations[1] + calculations[2] + slack
 
     total = total + sum
 
 print(total)
-
