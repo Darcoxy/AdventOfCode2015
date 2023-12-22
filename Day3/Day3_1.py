760ea0b92590a6d418a31d077be4b55505cc7b69
JJ <jj@anbast.com>
2023-12-22 12:09:32 +0000

    changes to code

--- Day3/Day3_1.py
+++ Day3/Day3_1.py
@@ -2,23 +2,20 @@ moves = []
 xCoordinate = 0
 yCoordinate = 0
 coordinatesList = []
-fullCoordinate = str(xCoordinate) + ',' + str(yCoordinate)
-coordinatesList[0] = fullCoordinate
+fullCoordinate = coordinatesList.append(str(xCoordinate) + ',' + str(yCoordinate))
 
-with open(r"C:\Users\postl\Desktop\Personal\Projects\AdventOfCode2015\Day3\input.txt") as my_file:
+with open(r"C:\Users\JJ\Desktop\Projects\AdventOfCode2015\Day3\input.txt") as my_file:
     for character in my_file:
         moves = list(character)
 
 for element in moves:
-    count = 1
     if element == '>':
         xCoordinate =+ 1
     elif element == '<':
         xCoordinate = xCoordinate - 1
     elif element == '^':
-        yCoordinate =+ 1
+        yCoordinate = yCoordinate + 1
     else: yCoordinate = yCoordinate - 1
     fullCoordinate = str(xCoordinate) + ',' + str(yCoordinate)
-    coordinatesList[count] = fullCoordinate
-    count =+ 1
-    print(coordinatesList)
+    coordinatesList.append(fullCoordinate)
+print(len(set(coordinatesList)))
