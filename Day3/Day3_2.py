b911caa258779cecf3dfe7667671557f5c5ba4f8
JJ <jj@anbast.com>
2023-12-22 14:45:33 +0000

    split the lists for santa and robosanta

---
+++ Day3/Day3_2.py
@@ -0,0 +1,23 @@
+moves = []
+xCoordinate = 0
+yCoordinate = 0
+coordinatesList = []
+fullCoordinate = coordinatesList.append(str(xCoordinate) + ',' + str(yCoordinate))
+
+with open(r"C:\Users\JJ\Desktop\Projects\AdventOfCode2015\Day3\input.txt") as my_file:
+    for character in my_file:
+        moves = list(character)
+
+for element in moves:
+    if element == '>':
+        xCoordinate = xCoordinate + 1
+    elif element == '<':
+        xCoordinate = xCoordinate - 1
+    elif element == '^':
+        yCoordinate = yCoordinate + 1
+    else: yCoordinate = yCoordinate - 1
+    fullCoordinate = str(xCoordinate) + ',' + str(yCoordinate)
+    coordinatesList.append(fullCoordinate)
+
+roboSantaCoordinates = coordinatesList[::2]
+santaCoordinates = coordinatesList[1::2]
