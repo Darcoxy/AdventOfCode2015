9d91d16a019f2d1c6377c906d56b233c513daa1d
JJ Lewandowski <post.lewandowski@gmail.com>
2023-12-23 12:51:14 +0000

    programmed first test case

--- Day4/Day4_1.py
+++ Day4/Day4_1.py
@@ -1,7 +1,26 @@
 import hashlib
 
-input = 'abcdef609043'
-target = '00000'
+def findHash(valueToHash):
+    result = hashlib.md5(valueToHash.encode())
+    return result.hexdigest()
 
-result = hashlib.md5(input.encode())
-print(result.hexdigest())
+def createCandidate(input, number):
+    candidate = input + str(number)
+    return candidate
+
+input = 'abcdef'
+# target = '00000'
+count = 0
+firstCharacters = ''
+
+while firstCharacters != '00000':
+    candidate = createCandidate(input, count)
+    hashed = findHash(candidate)
+    firstCharacters = hashed[0:5]
+    print(count)
+    if firstCharacters == '00000':
+        print(candidate)
+        print(hashed)
+        break
+    else:
+        count += 1
